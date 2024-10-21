a19881180c1be1c6f81ddac53255e641b94af300
Zuzana Čechová <zu.cechova9@gmail.com>
2024-10-21 19:36:29 +0200

    ukol

---
+++ index.js
@@ -0,0 +1,18 @@
+const bodyElement = document.querySelector("body")
+bodyElement.style.backgroundColor = "#e9e9e9"
+
+const classNewElement = document.querySelector(".news")
+classNewElement.style.backgroundColor = "white"
+classNewElement.style.width = "60rem"
+
+const nadpis = document.querySelector("h1")
+nadpis.className = "news__title"
+nadpis.textContent = "Aktuální novinky"
+
+const zprava1 = document.querySelector("#zprava1")
+zprava1.className = "post--main"
+
+document.querySelector("#zprava3 img").src = images/zprava3-novy.jpg 
+
+
+
